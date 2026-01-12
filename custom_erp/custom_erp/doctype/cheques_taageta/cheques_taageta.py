fb471007a8a7bedb1635809b102237a74b54230f
ghimirerohan <ghimirerohan@gmail.com>
2026-01-12 16:25:29 +0545

    fix: add missing Python files for DocTypes
    Added __init__.py and controller files for:
    - Institute
    - Cheques Taageta
    - Fonepay QR Transaction
    
    These are required for Frappe to load the DocType modules during migrate.

---
+++ custom_erp/custom_erp/doctype/cheques_taageta/cheques_taageta.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Rohan Ghimire and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class ChequesTaageta(Document):
+    pass
